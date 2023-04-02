532d97fc0b66f19d9ffe2ff059ce8e386ecd3e83
Joel Witherspoon <jwwitherspoon@gmail.com>
2023-04-01 23:41:38 -0400

    Get it working

---
+++ beefify.js
@@ -0,0 +1,18 @@
+function beefify(imageUrl) {
+    // Load canvas context
+    const canvas = document.getElementById('canvas').getContext('2d');
+
+    // Load and draw beef
+    const beef = new Image();
+    beef.addEventListener('load', () => {
+        canvas.drawImage(beef, 0, 0);
+
+        // Load and draw overlay when beef is drawn
+        const overlay = new Image();
+        overlay.addEventListener('load', () => {
+            canvas.drawImage(overlay, 32, 32, 32, 32);
+        });
+        overlay.src = imageUrl;
+    });
+    beef.src = 'beef.png';
+}
